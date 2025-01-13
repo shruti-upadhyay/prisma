cee4f5aae4d7e171905cf111fcf9e03e082efee3
shruti-upadhyay <shrutiupadhyay601@gmail.com>
2025-01-13 10:03:24 +0530

    commit to day-5

---
+++ day-5/src/user/user.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Post, Get, Patch, Delete, Body, Param } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() body) {
+    return this.userService.create(body);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return this.userService.update(id, body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(id);
+  }
+}
---
+++ day-5/src/user/user.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  create(data: { name: string; email: string; password: string; age: number }) {
+    return this.prisma.user.create({ data });
+  }
+
+  findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  findOne(id: string) {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+
+  update(id: string, data: { name?: string; email?: string; password?: string; age?: number }) {
+    return this.prisma.user.update({ where: { id }, data });
+  }
+
+  remove(id: string) {
+    return this.prisma.user.delete({ where: { id } });
+  }
+}
